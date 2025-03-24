60d7ede53c99de29e6e5965e119116dc0413a341
Nusret Efe Ucer <88771804+efe-u@users.noreply.github.com>
2025-03-24 17:53:33 -0500

    Update and rename job-submit-equil.sh to src/job-submit-equil.sh

--- src/job-submit-equil.sh
+++ src/job-submit-equil.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#SBATCH --job-name=UcerLab6
+#SBATCH --job-name=equilibrium
 #SBATCH --output=2L9N-equil
 #SBATCH --time=04:00:00
 #SBATCH --exclusive
 #SBATCH --partition=caslake
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=48
-#SBATCH --account=bios10603
+#SBATCH --account=
 
 module load namd/2.14
 
