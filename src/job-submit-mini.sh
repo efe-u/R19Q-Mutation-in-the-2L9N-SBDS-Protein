b4408e909b2b916f4c38e73525d4ec7eb6351038
Nusret Efe Ucer <88771804+efe-u@users.noreply.github.com>
2025-03-24 17:54:03 -0500

    Update and rename job-submit-mini.sh to src/job-submit-mini.sh

--- src/job-submit-mini.sh
+++ src/job-submit-mini.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#SBATCH --job-name=UcerLab6
+#SBATCH --job-name=minimization
 #SBATCH --output=2L9N-mini
 #SBATCH --time=00:10:00
 #SBATCH --exclusive
 #SBATCH --partition=caslake
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=48
-#SBATCH --account=bios10603
+#SBATCH --account=
 
 module load namd/2.14
 
