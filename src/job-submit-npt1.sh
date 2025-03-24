7f9ec15a0c3994e74e17030125008e45c2774c93
Nusret Efe Ucer <88771804+efe-u@users.noreply.github.com>
2025-03-24 17:54:26 -0500

    Update and rename job-submit-npt1.sh to src/job-submit-npt1.sh

--- src/job-submit-npt1.sh
+++ src/job-submit-npt1.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#SBATCH --job-name=UcerLab6
+#SBATCH --job-name=npt-equilibrium
 #SBATCH --output=2L9N_R19Q-NPT
 #SBATCH --time=10:00:00
 #SBATCH --exclusive
 #SBATCH --partition=caslake
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=48
-#SBATCH --account=bios10603
+#SBATCH --account=
 
 module load namd/2.14
 
